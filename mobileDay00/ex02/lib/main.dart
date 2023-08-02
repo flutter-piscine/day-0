e7fe4edb69a77d56abc8c3afb9481f66f63dd50b
Victor Wildner <vcwildm@gmail.com>
2023-08-02 17:59:04 -0300

    feat: add ex02

---
+++ mobileDay00/ex02/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  TextButton customButton(String text, Color color) {
+    return TextButton(
+      onPressed: () {
+        print(text);
+      },
+      style: TextButton.styleFrom(
+        foregroundColor: color,
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a blue toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Calculator'),
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 6,
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          '0',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        Text(
+                          '0',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        Text(
+                          '',
+                          style: TextStyle(fontSize: 48),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                color: const Color.fromARGB(255, 110, 152, 187),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        customButton('7', Colors.black),
+                        customButton('8', Colors.black),
+                        customButton('9', Colors.black),
+                        customButton('C', Colors.red),
+                        customButton('AC', Colors.red),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        customButton('4', Colors.black),
+                        customButton('5', Colors.black),
+                        customButton('6', Colors.black),
+                        customButton('+', Colors.white),
+                        customButton('-', Colors.white),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        customButton('1', Colors.black),
+                        customButton('2', Colors.black),
+                        customButton('3', Colors.black),
+                        customButton('x', Colors.white),
+                        customButton('/', Colors.white),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        customButton('0', Colors.black),
+                        customButton('.', Colors.black),
+                        customButton('00', Colors.black),
+                        customButton('=', Colors.white),
+                        customButton('', Colors.red),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
